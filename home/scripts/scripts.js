be09a11e46ac9e86764e2307623e1c9855dd1b9d
charonchan <46700458+charonchan@users.noreply.github.com>
2019-04-17 19:26:51 -0400

    javaScript on Services

--- home/scripts/scripts.js
+++ home/scripts/scripts.js
@@ -4,7 +4,6 @@ console.log("Hello World!");
 var elMainMenu = document.getElementById('menu');
 var elHambBtn = document.querySelector('.hamburger');
 
-
 elHambBtn.addEventListener('click', function(e) {
   elMainMenu.classList.toggle('close');
 });
--- landlords/scripts/scripts.js
+++ landlords/scripts/scripts.js
@@ -1,1 +1,20 @@
 console.log("Hello World!");
+
+var elServicesRental = document.getElementById('listing');
+var elServicesManage = document.getElementById('management');
+var elListingBtn = document.querySelector('.rental-listing');
+var elManageBtn = document.querySelector('.rental-manage');
+
+
+elServicesRental.style.display = 'none';
+elServicesManage.style.display = 'none';
+
+elListingBtn.addEventListener('click', function(e) {
+  elServicesRental.style.display = 'grid';
+  elServicesManage.style.display = 'none';
+});
+
+elManageBtn.addEventListener('click', function(e) {
+  elServicesRental.style.display = 'none';
+  elServicesManage.style.display = 'grid';
+});
