401d5d4483ca97d4b63551d190521266c8b46be4
charonchan <46700458+charonchan@users.noreply.github.com>
2019-04-17 21:35:01 -0400

    changed ID

--- landlords/scripts/scripts.js
+++ landlords/scripts/scripts.js
@@ -6,9 +6,6 @@ var elListingBtn = document.querySelector('.rental-listing');
 var elManageBtn = document.querySelector('.rental-manage');
 
 
-elServicesRental.style.display = 'none';
-elServicesManage.style.display = 'none';
-
 elListingBtn.addEventListener('click', function(e) {
   elServicesRental.style.display = 'grid';
   elServicesManage.style.display = 'none';
@@ -18,3 +15,11 @@ elManageBtn.addEventListener('click', function(e) {
   elServicesRental.style.display = 'none';
   elServicesManage.style.display = 'grid';
 });
+
+// elListingBtn.addEventListener('click', function(e) {
+//   elServicesRental.classList.toggle('open');
+// });
+//
+// elManageBtn.addEventListener('click', function(e) {
+//   elServicesManage.classList.toggle('open');
+// });
